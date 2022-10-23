e027c241cbe9cfdfaca317c600e0163412232ff5
ivan <ivanni95@gmail.com>
2022-10-23 21:18:41 +0400

    Решил 4 задачу

--- src/Main.java
+++ src/Main.java
@@ -27,10 +27,28 @@ public class Main {
         System.out.println(b[2] + "," + b[1] + "," + b[0]);
         System.out.println(c[3] + "," + c[2] + "," + c[1] + "," + c[0]);
 
+        // Задача 4
+        for (int i = 0; i < 3; i++) {
+            a[i] = a[i] + 1;
+            if (a[i]%2==0)
+
+
+                System.out.println(a[i]);
+
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
 
 
 
             
-        }
 
-    }
