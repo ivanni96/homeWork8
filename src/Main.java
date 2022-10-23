ce50b305a3ee59443c4a77a49f473f6626759b66
ivan <ivanni95@gmail.com>
2022-10-23 21:16:28 +0400

    Решил 1 задачу

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        int[] a = new int[3];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+
+        System.out.println(a[1]);
+
+        double[] b = {1.57, 7.645, 9.986};
+
+        System.out.println(b[2]);
+
+        int[] c = {1, 6, 8, 7, 9};
+        System.out.println(c[3]);
+
+
+
+
+            
+        }
+
+    }
